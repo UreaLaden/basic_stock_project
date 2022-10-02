4dcb529ef2312c8316f15158a2a3a77ce29f3eac
Leaundrae Mckinney <pytechdrae@gmail.com>
2022-10-02 16:00:23 -0400

    Fixed bug with display chart and implemented csv imports

--- daily_data.py
+++ daily_data.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 import matplotlib.pyplot as plt
 
 class DailyData():    
@@ -7,10 +7,49 @@ class DailyData():
         self.date = date
         self.closing_price = closing_price
         self.volume = volume
+    
+    def __str__(self):
+        return (f'DailyData({self.date},{self.closing_price},{self.volume})')
+    
+    def __lt__(self,other):
+        myStrDate = str(self.date)
+        oStrDate = str(other.date)
         
-    # Display Report 
-    def display_report(self):
-        print("This method is under construction")
+        mySplitDate = myStrDate.split('-')
+        oSplitDate = oStrDate.split('-')
+        
+        mYear = int(mySplitDate[0])
+        mMonth = int(mySplitDate[1])
+        mDay = int(mySplitDate[2])
+        mDate = date(mYear,mMonth,mDay)
+        
+        oYear = int(oSplitDate[0])
+        oMonth = int(oSplitDate[1])
+        oDay = oSplitDate[2] if len(oSplitDate) < 4 else int(oSplitDate[2].split(' ')[0])
+        oDate = date(oYear,oMonth,int(oDay))
+
+        return self if mDate < oDate else other
+    
+    def __gt__(self,other):
+        myStrDate = str(self.date)
+        oStrDate = str(other.date)
+        
+        mySplitDate = myStrDate.split('-')
+        oSplitDate = oStrDate.split('-')
+        
+        mYear = int(mySplitDate[0])
+        mMonth = int(mySplitDate[1])
+        mDay = int(mySplitDate[2])
+        mDate = date(mYear,mMonth,mDay)
+        
+        oYear = int(oSplitDate[0])
+        oMonth = int(oSplitDate[1])
+        oDay = oSplitDate[2] if len(oSplitDate) < 4 else int(oSplitDate[2].split(' ')[0])
+        oDate = date(oYear,oMonth,int(oDay))
+
+        return self if mDate > oDate else other
+        
+
 
     
 
--- stock_class.py
+++ stock_class.py
@@ -5,11 +5,15 @@ Created on Mon Sep 19 2022
 @author: Leaundrae Mckinney
 """
 
+from email import header
+import math
 from daily_data import DailyData
 from datetime import datetime
 from tabulate import tabulate
 import account_class as accounts
 from matplotlib import pyplot as plt
+import csv,glob,sys
+from os import system,name
 
 class Stock:
 
@@ -94,7 +98,7 @@ class StockData:
                 return
     # Pause for user confirmation to continue
     def delay(self):
-        _ = input('Press Enter to Continue ***')
+        _ = input('\t\t--- Press Enter to Continue ---')
     # Instantiate Traditional or Robo account depending on user inputq
     def investment_type(self):
         print("Investment Account ---")
@@ -131,21 +135,51 @@ class StockData:
             trad_acct.add_stock(temp_list)
     # Get price and volume history from Yahoo! Finance using CSV import.
     def import_stock_csv(self):
-        print("This method is under construction")
+        print("----------Import historical Stock data from file---------")
+        stock_files = glob.glob('./*.csv')
+        availableImports = [i.split('.')[1].strip('\\') for i in stock_files]
+        symbol = input("Enter data for import: " + str(availableImports) + ": ").upper()
+        if symbol not in availableImports:
+            print('\nInvalid entry please try again\n')
+            return
+        dataCount = 0
+        foundSymbol = False
+        with open(symbol + '.csv','r') as data:
+            symbol = data.name.split('.')[0]
+            reader = csv.reader(data,delimiter=',')
+            for idx,row in enumerate(reader):
+                if(idx > 0):
+                    date = row[0]
+                    close = row[4]
+                    volume = row[-1]
+                    if len(self.stock_list) <= 0:
+                        # If we don't have the selected Stock at to our list
+                        self.stock_list.append(Stock(symbol,symbol))
+                    for stock in self.stock_list:
+                        if stock.symbol == symbol:
+                            foundSymbol = True
+                            stock.daily_data.append(DailyData(date,close,volume))
+                    dataCount = idx
+            if foundSymbol:
+                print(f'Imported {dataCount} rows of data')
+        self.display_report()
+
+                    
     # Function to create stock chart
     def display_stock_chart(self,symbol):
-        dates = []
-        prices = []
-        volumes = [] 
+        dates:list[datetime] = []
+        prices:list[float] = []
+        volumes:list[int] = [] 
         company = ""
         for stock in self.stock_list:
             if(stock.symbol == symbol):
                 company = stock.company_name
                 for data in stock.daily_data:
-                    dates.append(data.date)
-                    prices.append(data.closing_price)
+                    new_date = datetime.strptime(data.date,'%Y-%m-%d')
+                    dates.append(new_date)
+                    prices.append(float(data.closing_price))
                     volumes.append(data.volume)
-        plt.plot(dates,prices,'ro-',linewidth=5,markersize=5,alpha=0.35)
+        plt.plot(dates,prices,'g--',linewidth=1)
         plt.xlabel('Date')
         plt.ylabel('Price')
         plt.title(company)
@@ -163,3 +197,72 @@ class StockData:
         else:
             print(f'Symbol: {symbol} not found')
             self.delay()
+    # Display Report 
+    def display_report(self):
+        self.clear_screen()
+        for stock in self.stock_list:
+            
+            count = 0
+            price_total = 0.0
+            volume_total = 0
+            lowPrice = sys.maxsize
+            highPrice = 0.0
+            lowVolume = sys.maxsize
+            highVolume = 0.0
+            max_date = str(datetime.max).split(' ')[0]
+            min_date = str(datetime.min).split(' ')[0]
+            earliest_data = DailyData(max_date,0,0)
+            latest_data = DailyData(min_date,0,0)
+            price_change = 0.0
+            profit_loss = 0.0
+            # start_price = stock.start_price
+            for data in stock.daily_data:
+                count += 1
+                price_total += float(data.closing_price)
+                volume_total += int(data.volume)
+                
+                lowPrice = min(lowPrice,float(data.closing_price))
+                highPrice = max(highPrice,float(data.closing_price))
+                lowVolume = min(lowVolume,int(data.volume))
+                highVolume = max(highVolume,int(data.volume))
+                
+                earliest_data = data < earliest_data #Re-wrote __lt__ to return earliest data
+                latest_data = data > latest_data #Re-wrote __gt__ to return earliest data
+                price_change = float(latest_data.closing_price) - float(earliest_data.closing_price)
+                profit_loss = price_change * stock.num_shares
+            
+            if count > 0:
+                header_string = '\n\n\nReport for: {ticker}  Total Shares: {share_amt}'.format(ticker=stock.symbol,share_amt=stock.num_shares)
+                data_headers = [['   Date','Closing Price','Volume']] + [[i.date,'${:.2f}'.format(float(i.closing_price)),i.volume] for i in stock.daily_data]
+                low_price = '${:.2f}'.format(lowPrice)
+                high_price = '${:.2f}'.format(highPrice)
+                avg_price = '${:.2f}'.format(price_total / count)
+                
+                price_headers = [['Low Price','High Price','Average Price']]  
+                price_stats = [[low_price,high_price,avg_price]]
+                volume_headers = [['Low Volume','High Volume','Average Volume']]
+                avg_vol = math.ceil(volume_total / count)
+                volume_stats = [[lowVolume,highVolume,avg_vol]]
+                
+                earning_headers = [['Start','End','$ Change','Profit/Loss']]
+                start_price = '${:.2f}'.format(float(earliest_data.closing_price))
+                end_price = '${:.2f}'.format(float(latest_data.closing_price))
+                earning_stats = [[start_price,end_price,price_change,'{:.2f}'.format(profit_loss)]]
+                
+                print(header_string,'\n')
+                print(tabulate(data_headers,headers='firstrow',numalign='center'))
+                print('\n-------------------Summary-------------------\n')
+                print(tabulate(price_headers + price_stats,headers='firstrow',numalign='center'))
+                print('\n',tabulate(volume_headers + volume_stats,headers='firstrow',numalign='center'))
+                print('\n',tabulate(earning_headers+earning_stats,headers='firstrow',numalign='center'))     
+            else:
+                print('No daily history')
+
+            print('\n\n\n\t\t--- Report Complete ---')       
+            self.delay()
+
+    def clear_screen(self):
+        if name == 'nt':
+            _ = system('cls')
+        else:
+            _ = system('clear')
